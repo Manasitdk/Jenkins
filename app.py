6b2833c6c56c18a2c5c3ae0b8ee8923055755535
Manasitdk <manasitdk@gmail.com>
2024-04-17 12:56:06 +0530

    6 update in python file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Manasi Tidke'
+    return 'Hello, Jenkins..'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
